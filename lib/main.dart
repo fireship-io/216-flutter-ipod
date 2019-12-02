f3ac912e72cad512328194dd9530a1364fa3751c
unknown <hello@jeffdelaney.me>
2019-12-02 09:15:19 -0700

    make buttons work

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ List images = [
   'https://www.covercentury.com/covers/audio/b/baby_justin-bieber_218-vbr_1462296.jpg',
   'https://thebrag.com/wp-content/uploads/2015/10/artvsscienceofftheedgeoftheearthandintoforeverforever1015.jpg',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS95jA18s6vnmWIk4z6jTuaINYm4jBoWWRC5yp7cXvwKikEASRR&s'
-      'https://list.lisimg.com/image/2239608/500full.jpg',
+  'https://list.lisimg.com/image/2239608/500full.jpg',
   'https://images-na.ssl-images-amazon.com/images/I/513D1NEWPXL.jpg'
 ];
 
@@ -35,16 +35,15 @@ class IPod extends StatefulWidget {
 }
 
 class _IPodState extends State<IPod> {
-
-  var ctrl = PageController(viewportFraction: 0.75);
+  final PageController _pageCtrl = PageController(viewportFraction: 0.6);
 
   double currentPage = 0.0;
 
   @override
   void initState() {
-    ctrl.addListener(() {
+    _pageCtrl.addListener(() {
       setState(() {
-        currentPage = ctrl.page;
+        currentPage = _pageCtrl.page;
       });
     });
     super.initState();
@@ -52,117 +51,135 @@ class _IPodState extends State<IPod> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 300,
-          color: Colors.black,
-          child: PageView.builder(
-            controller: ctrl,
-            scrollDirection: Axis.horizontal,
-            itemCount: 9, //Colors.accents.length,
-            itemBuilder: (context, int currentIdx) {
-              return AlbumCard(
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            color: Colors.black,
+            child: PageView.builder(
+              controller: _pageCtrl,
+              scrollDirection: Axis.horizontal,
+              itemCount: 9, //Colors.accents.length,
+              itemBuilder: (context, int currentIdx) {
+                return AlbumCard(
                   color: Colors.accents[currentIdx],
                   idx: currentIdx,
-                  currentPage: currentPage);
-            },
+                  currentPage: currentPage,
+                );
+              },
+            ),
           ),
-        ),
-        Spacer(),
-        Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              GestureDetector(
-                onPanUpdate: _panHandler,
-                child: Container(
-                  height: 300,
-                  width: 300,
+          Spacer(),
+          Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                GestureDetector(
+                  onPanUpdate: _panHandler,
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                    child: Stack(children: [
+                      Container(
+                        child: Text(
+                          'MENU',
+                          style: TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        alignment: Alignment.topCenter,
+                        margin: EdgeInsets.only(top: 36),
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(Icons.fast_forward),
+                          iconSize: 40,
+                          onPressed: () => _pageCtrl.animateToPage(
+                           (_pageCtrl.page + 1).toInt(),
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.easeIn
+                            ),
+                        ),
+                        alignment: Alignment.centerRight,
+                        margin: EdgeInsets.only(right: 30),
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(Icons.fast_rewind),
+                          iconSize: 40,
+                          onPressed: () => _pageCtrl.animateToPage(
+                           (_pageCtrl.page - 1).toInt(),
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.easeIn
+                            ),
+                        ),
+                        alignment: Alignment.centerLeft,
+                        margin: EdgeInsets.only(left: 30),
+                      ),
+                      Container(
+                        child: Icon(
+                          Icons.play_arrow,
+                          size: 40,
+                        ),
+                        alignment: Alignment.bottomCenter,
+                        margin: EdgeInsets.only(bottom: 30),
+                      )
+                    ]),
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
-                    color: Colors.black,
+                    color: Colors.white38,
                   ),
-                  child: Stack(children: [
-                    Container(
-                      child: Text(
-                        'MENU',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                      alignment: Alignment.topCenter,
-                      margin: EdgeInsets.only(top: 36),
-                    ),
-                    Container(
-                      child: Icon(
-                        Icons.fast_forward,
-                        size: 40,
-                      ),
-                      alignment: Alignment.centerRight,
-                      margin: EdgeInsets.only(right: 30),
-                    ),
-                    Container(
-                      child: Icon(
-                        Icons.fast_rewind,
-                        size: 40,
-                      ),
-                      alignment: Alignment.centerLeft,
-                      margin: EdgeInsets.only(left: 30),
-                    ),
-                    Container(
-                      child: Icon(
-                        Icons.play_arrow,
-                        size: 40,
-                      ),
-                      alignment: Alignment.bottomCenter,
-                      margin: EdgeInsets.only(bottom: 30),
-                    )
-                  ]),
                 ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white38,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        Spacer(),
-      ],
+          Spacer(),
+        ],
+      ),
     );
   }
 
   void _panHandler(DragUpdateDetails d) {
-    bool isRight = d.delta.dx >= 0.0;
-    bool isUp = d.delta.dy <= 0.0;
-    bool isLeft = !isRight;
-    bool isDown = !isUp;
 
+    /// Pan movements
+    bool panUp = d.delta.dy <= 0.0;
+    bool panLeft = d.delta.dx <= 0.0;
+    bool panRight = !panLeft;
+    bool panDown = !panUp;
+
+    /// Pan location on the wheel
     bool onTop = d.localPosition.dy <= 150;
     bool onLeftSide = d.localPosition.dx <= 150;
     bool onRightSide = !onLeftSide;
     bool onBottom = !onTop;
 
-    // Absoulte change on axis
+    /// Absoulte change on axis
     double yChange = d.delta.dy.abs();
     double xChange = d.delta.dx.abs();
 
-    // Directional change on wheel
-    double vert = (onRightSide && isUp) || (onLeftSide && isDown)
+    /// Directional change on wheel
+    double vert = (onRightSide && panUp) || (onLeftSide && panDown)
         ? yChange
         : yChange * -1;
 
-    double horz =
-        (onTop && isLeft) || (onBottom && isRight) ? xChange : xChange * -1;
+    double horz = (onTop && panLeft) || (onBottom && panRight) 
+        ? xChange 
+        : xChange * -1;
 
-    //
-    ctrl.jumpTo(
-      (ctrl.offset + ((horz + vert) * (d.delta.distance * 0.2))),
-    );
+    // Total computed change with velocity
+    double scrollOffsetChange = (horz + vert) * (d.delta.distance * 0.2);
+
+    // Move the page view scroller 
+    _pageCtrl.jumpTo(_pageCtrl.offset + scrollOffsetChange);
   }
 }
 
@@ -178,8 +195,12 @@ class AlbumCard extends StatelessWidget {
 
     return Container(
       width: 250,
-      child: Transform.scale(
-        scale: (1 - relativePosition.abs()).clamp(0.2, 0.6) + 0.4,
+      child: Transform(
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.003) // add perspective
+          ..scale((1 - relativePosition.abs()).clamp(0.2, 0.6) + 0.4)
+          ..rotateY(relativePosition),
+        // ..rotateZ(relativePosition),
         alignment: relativePosition >= 0
             ? Alignment.centerLeft
             : Alignment.centerRight,
